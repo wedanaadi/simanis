6493f8f30de93db655ce0eda26020f81c4cfe65f
putuartatkj <42176158+putuartatkj@users.noreply.github.com>
2018-11-04 18:55:54 +0800

    profil
    profil

---
+++ application/controllers/Profil.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profil extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('profil/dataprofil');
+	}
+
+}
+
+/* End of file Profil.php */
+/* Location: ./application/controllers/Profil.php */
+ ?>
